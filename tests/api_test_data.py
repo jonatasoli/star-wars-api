8e78c14cbf4baed5913a0e09c390e36a2e43e747
Jônatas Luiz de Oliveira <contact@jonatasoliveira.dev>
2022-03-12 08:28:06 -0300

    separate searchs in functions

--- tests/api_test_data.py
+++ tests/api_test_data.py
@@ -1,11 +1,5 @@
-from typing import Optional
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
 yavin_dict = dict(
-name='Yavin IV',
+    name='Yavin IV',
     climate='temperate, tropical',
     diameter='10200',
     population='1000',
@@ -23,16 +17,3 @@ alderaan_dict = dict(
 )
 
 list_planets = [yavin_dict, alderaan_dict]
-
-@app.get('/planets')
-def get_planets(search: str = None):
-    if search:
-        return search_planet(search)
-    return list_all_planets()
-
-
-def search_planet(search: str):
-    return yavin_dict
-
-def list_all_planets():
-    return list_planets
--- tests/test_planets.py
+++ tests/test_planets.py
@@ -1,25 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-
-yavin_dict = dict(
-    name='Yavin IV',
-    climate='temperate, tropical',
-    diameter='10200',
-    population='1000',
-    films=[dict(title='A New Hope', release_date='1977-05-25')],
-)
-alderaan_dict = dict(
-    name='Alderaan',
-    climate='temperate',
-    diameter='12500',
-    population='2000000000',
-    films=[
-        dict(title='A New Hope', release_date='1977-05-25'),
-        dict(title='Revenge of the Sith', release_date='2005-05-19'),
-    ],
-)
-
-list_planets = [yavin_dict, alderaan_dict]
+from .api_test_data import list_planets, yavin_dict
 
 
 def test_status_code_planet_endpoint(client):
